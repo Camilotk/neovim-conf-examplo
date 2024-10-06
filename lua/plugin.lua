29b0bde06ab3ced743b80808e1d0fca6cdace707
Camilo Cunha de Azevedo <camilotk@gmail.com>
2024-10-06 05:55:10 -0300

    Create plugin.lua

---
+++ lua/plugin.lua
@@ -0,0 +1,18 @@
+-- lua/plugin.lua
+-- Arquivo que contem as configurações do Gerenciador de Plugins Lazy.
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Inicializar Lazy.nvim
+require("lazy").setup("plugins")
