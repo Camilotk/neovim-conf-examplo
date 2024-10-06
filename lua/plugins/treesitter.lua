b29f77623f14943b08660767918379a0a81edcb0
Camilo Cunha de Azevedo <camilotk@gmail.com>
2024-10-06 05:56:23 -0300

    Create treesitter.lua

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "c" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
+  },
+}
